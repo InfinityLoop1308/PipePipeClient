c198fa7a146ff134f927b37d5334a71d9eea0c77
kedwjao <96324692+kedwjao@users.noreply.github.com>
2022-06-16 20:04:52 +0800

    merge: merge upstream pr #8443 comment replies.

--- app/src/main/java/org/schabi/newpipe/info_list/holder/CommentsInfoItemHolder.java
+++ app/src/main/java/org/schabi/newpipe/info_list/holder/CommentsInfoItemHolder.java
@@ -4,6 +4,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.schabi.newpipe.R;
 import org.schabi.newpipe.extractor.InfoItem;
@@ -35,13 +36,16 @@ public class CommentsInfoItemHolder extends CommentsMiniInfoItemHolder {
     public final TextView itemTitleView;
     private final ImageView itemHeartView;
     private final ImageView itemPinnedView;
-
+    private final RepliesHandler repliesHandler;
     public CommentsInfoItemHolder(final InfoItemBuilder infoItemBuilder, final ViewGroup parent) {
         super(infoItemBuilder, R.layout.list_comments_item, parent);
 
         itemTitleView = itemView.findViewById(R.id.itemTitleView);
         itemHeartView = itemView.findViewById(R.id.detail_heart_image_view);
         itemPinnedView = itemView.findViewById(R.id.detail_pinned_view);
+        final TextView showReplies = itemView.findViewById(R.id.showReplies);
+        final RecyclerView repliesView = itemView.findViewById(R.id.replyRecycleView);
+        repliesHandler = new RepliesHandler(showReplies, repliesView);
     }
 
     @Override
@@ -53,11 +57,9 @@ public class CommentsInfoItemHolder extends CommentsMiniInfoItemHolder {
             return;
         }
         final CommentsInfoItem item = (CommentsInfoItem) infoItem;
-
         itemTitleView.setText(item.getUploaderName());
-
         itemHeartView.setVisibility(item.isHeartedByUploader() ? View.VISIBLE : View.GONE);
-
         itemPinnedView.setVisibility(item.isPinned() ? View.VISIBLE : View.GONE);
+        repliesHandler.checkForReplies(item);
     }
 }
