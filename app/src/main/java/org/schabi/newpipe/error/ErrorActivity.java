1cffa1db21c8e1c72a1400534ef9ef0b2bd82c8b
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2024-07-20 14:03:44 +0800

    UI: use GitHub URLs

--- app/src/main/java/org/schabi/newpipe/error/ErrorActivity.java
+++ app/src/main/java/org/schabi/newpipe/error/ErrorActivity.java
@@ -72,7 +72,7 @@ public class ErrorActivity extends AppCompatActivity {
     public static final String ERROR_EMAIL_SUBJECT = "Exception in ";
 
     public static final String ERROR_GITHUB_ISSUE_URL
-            = "https://codeberg.org/NullPointerException/PipePipe/issues";
+            = "https://github.com/InfinityLoop1308/PipePipe/issues";
 
     public static final DateTimeFormatter CURRENT_TIMESTAMP_FORMATTER
             = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
