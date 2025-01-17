31ded32649480c9f101552da0e6dc7ce55ad7979
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2025-01-18 06:11:52 +0800

    Revert "behavior: add a setting for user to select whether or not using yt-dlp"
    This reverts commit 0323c711

--- app/src/main/java/org/schabi/newpipe/util/ServiceHelper.java
+++ app/src/main/java/org/schabi/newpipe/util/ServiceHelper.java
@@ -282,12 +282,16 @@ public final class ServiceHelper {
                     R.string.youtube_cookies_key), null);
             final String audioLanguage = sharedPreferences.getString(context.getString(
                     R.string.preferred_audio_language_key),"original");
-            final boolean useYtdlp = sharedPreferences.getBoolean(context.getString(
-                    R.string.enable_compatibility_mode_key),false);
+            final String proxyToken = sharedPreferences.getString(context.getString(R.string.proxy_token_key), "");
+            final Boolean proxyEnabled = sharedPreferences.getBoolean(context.getString(R.string.enable_proxy_key), false);
             ServiceList.YouTube.setTokens(tokens);
+            if (proxyEnabled) {
+                ServiceList.YouTube.setTokens(null);
+                ServiceList.YouTube.setProxyToken(proxyToken);
+                ServiceList.YouTube.setProxyEnabled(true);
+            }
             ServiceList.YouTube.setContentLanguage(getPreferredLocalization(context));
             ServiceList.YouTube.setAudioLanguage(audioLanguage);
-            ServiceList.YouTube.setYtdlpEnabled(useYtdlp);
 //            final String pot = sharedPreferences.getString(context.getString(R.string.youtube_po_token), null);
 //            ServiceList.YouTube.setAdditionalTokens(pot);
             if(sharedPreferences.getBoolean(context.getString(R.string.override_cookies_youtube_key), false)) {
