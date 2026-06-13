ea4cdb419a6a1bba3d9830751784ac23becbad08
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-06-14 07:35:59 +0800

    Fix feed receiver registration

--- app/src/main/java/org/schabi/newpipe/local/feed/service/FeedLoadService.kt
+++ app/src/main/java/org/schabi/newpipe/local/feed/service/FeedLoadService.kt
@@ -222,7 +222,11 @@ class FeedLoadService : Service() {
                 }
             }
         }
-        registerReceiver(broadcastReceiver, IntentFilter(ACTION_CANCEL))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(broadcastReceiver, IntentFilter(ACTION_CANCEL), Context.RECEIVER_NOT_EXPORTED)
+        } else {
+            registerReceiver(broadcastReceiver, IntentFilter(ACTION_CANCEL))
+        }
     }
 
     // /////////////////////////////////////////////////////////////////////////
