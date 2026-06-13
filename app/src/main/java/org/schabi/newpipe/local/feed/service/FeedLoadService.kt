44990e182d17b6d3ab07ee84813447e65ea77a35
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-06-14 07:49:15 +0800

    Fix feed load completion crash

--- app/src/main/java/org/schabi/newpipe/local/feed/service/FeedLoadService.kt
+++ app/src/main/java/org/schabi/newpipe/local/feed/service/FeedLoadService.kt
@@ -95,20 +95,12 @@ class FeedLoadService : Service() {
             .doOnSubscribe {
                 startForeground(NOTIFICATION_ID, notificationBuilder.build())
             }
-            .subscribe { _, error ->
-                // There seems to be a bug in the kotlin plugin as it tells you when
-                // building that this can't be null:
-                // "Condition 'error != null' is always 'true'"
-                // However it can indeed be null
-                // The suppression may be removed in further versions
-                @Suppress("SENSELESS_COMPARISON")
-                if (error != null) {
-                    Log.e(TAG, "Error while storing result", error)
-                    handleError(error)
-                    return@subscribe
-                }
+            .subscribe({
                 stopService()
-            }
+            }, { error ->
+                Log.e(TAG, "Error while storing result", error)
+                handleError(error)
+            })
         return START_NOT_STICKY
     }
 
