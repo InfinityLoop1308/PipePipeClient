d2d8728c10b8ad4065fc351822e728d1c0c60277
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-06-20 17:18:30 +0800

    fix: disable edge-to-edge enforcement on Android 15+

--- app/src/main/java/org/schabi/newpipe/App.java
+++ app/src/main/java/org/schabi/newpipe/App.java
@@ -79,6 +79,7 @@ public class App extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        EdgeToEdgeWorkaround.apply();
 
         app = this;
 
