35fe0d812303393986054eb8b812fee2b2964e03
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-07-27 11:33:20 +0800

    fix: refine playback speed gesture strings

--- app/src/main/java/org/schabi/newpipe/player/event/BasePlayerGestureListener.kt
+++ app/src/main/java/org/schabi/newpipe/player/event/BasePlayerGestureListener.kt
@@ -101,11 +101,7 @@ abstract class BasePlayerGestureListener(
 
                 // Check if swiping up (negative y velocity)
                 if (yVelocity < 0) {
-                    v.parent.requestDisallowInterceptTouchEvent(
-                        player.isFullscreenGestureEnabled
-                            || player.isFullscreen
-                            || PlayerHelper.isPlaybackSpeedGestureEnabled(service)
-                    )
+                    v.parent.requestDisallowInterceptTouchEvent(player.isFullscreenGestureEnabled || player.isFullscreen)
                 } else {
                     v.parent.requestDisallowInterceptTouchEvent(player.isFullscreen)
                 }
